a423feaf2fbe297971824096e0dab96863c7a356
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-15 15:08:20 -0500

    initial set up of the server connections

---
+++ server.js
@@ -0,0 +1,34 @@
+// require dependencies
+const express = require("express");
+const path = require("path")
+const app = express();
+
+// port
+const PORT = process.env.PORT || 3000;
+
+// send to server
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+// routes to server
+
+// GET
+// Go to survey
+app.get("/survey", (req,res)=>{
+    console.log(req.method);
+    res.send("survey is open")
+})
+// Go to homepage
+app.get("/*",(req,res)=>{
+    console.log(req.mehotd);
+    res.send("Homepage")
+})
+// POST
+app.post("/survey",(req,res)=>{
+    console.log(req.body);
+    res.send("Post the survery results")
+})
+// server reposts
+app.listen(PORT, ()=>{
+    console.log("app listening on " + PORT);
+})
