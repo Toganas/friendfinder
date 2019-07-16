f85cb389afd547a26004f2a3477d5188b2cbf983
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-16 03:13:16 -0500

    after who knows how many hours of nothing changing and me changing paths only to have nothing work still, and continue trying.  I put the javascript inside survey.html and it worked.  Sometimes, life isn't fair

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,7 @@
-// require ajax
-const ajax = require("ajax");
 // array to have information pushed into
 let friendsArray = [{}];
 // exporting array
 module.exports = friendsArray;
 
-// capturing inputs
-$("#submit").on("click", (event) => {
-    event.preventDefault();
-
-    // Validating Form
-    validation = () => {
-        let goodForm = true;
-        $(".chosen-select").each(() => {
-            if (this.val() === "") {
-                goodForm = false;
-            }
-        })
-        return goodForm;
-    }
-    // if validation is true, capture all the data
-    if (validation()) {
-        let data = {
-            name: $("#name").val().trim(),
-            picture: $("#picture").val().trim(),
-            answers: [
-                $("#q1").val(),
-                $("#q2").val(),
-                $("#q3").val(),
-                $("#q4").val(),
-                $("#q5").val(),
-                $("#q6").val(),
-                $("#q7").val(),
-                $("#q8").val(),
-                $("#q9").val(),
-                $("#q10").val(),
-            ]
-        };
-        // $.post("/api/friends", data, (res) => {
-
-        //     $()
-        // })
-    }
-    else {
-        alert("You need to fill out all forms before submitting!")
-    }
-})
 
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,10 @@
+// requiring dependencies
 const path = require("path")
+const express = require("express")
+const app = express();
+
+// setting static
+app.use(express.static(path.join(__dirname, 'app')));
 
 module.exports = (app) =>{
 
@@ -6,11 +12,13 @@ module.exports = (app) =>{
 // Go to survey
 app.get("/survey", (req, res) => {
     console.log("go to Survey");
-    res.sendFile(path.join(__dirname, "../public/survey.html"))
+    res.sendFile(path.join(__dirname, "/public/survey.html"))
+    res.sendFile(path.join(__dirname, "/data/friends.js"))
 })
+
 // Go to homepage
 app.get("/*", (req, res) => {
     console.log("Go to homepage");
-    res.sendFile(path.join(__dirname, "app/public/home.html"))
+    res.sendFile(path.join(__dirname, "/public/home.html"))
 })
 }
