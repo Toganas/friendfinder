81f29edbe4cf011f4191d3b4a0338028339b03fe
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-16 20:57:34 -0500

    added in code that *should* be comparing friends.  It doesn't work.  I'm crying.  I think I've pulled out enough hair to make a cat

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,9 +9,36 @@ module.exports = (app) => {
 
     // POST
     app.post("/api/friends", (req, res) => {
-        friendsData.push(req.body);
-        console.log(req.body);
-        res.json(true);
+        let newFriend = {
+            name: "",
+            picture: "",
+            newDifference: 100000000
+        };
+
+        let data = req.body
+        let newScores = data.scores;
+        let newName = data.name;
+        let newPicture = data.picture;
+
+        let totalDifference = 0;
+
+        for (var i = 0; i < friendsData.length -1; i++){
+            totalDifference = 0;
+
+            for (var j=0; j <10; j++) {
+                totalDifference += Math.abs(parseInt(newScores[j] - parseInt(friendsData[j])));
+                if (totalDifference <= newFriend.newDifference) {
+                    newFriend.name = friendsData[i].name;
+                    newFriend.picture = friendsData[i].picture;
+                    newFriend.newDifference = totalDifference;
+                }
+            }
+        }
+
+
+        friendsData.push(data);
+        
+        res.json(newFriend);
     })
 
 }
